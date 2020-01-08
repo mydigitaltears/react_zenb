559beaa9dfcf5076e2c35aae5f7aadf12fadcd1e
Xavier <xavier.touikan@gmail.com>
2020-01-08 12:26:30 -0500

    initial commit

---
+++ src/Body.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Music from "./components/Music"
+import Videos from "./components/Videos"
+import Shows from "./components/Shows"
+import Contacts from "./components/Contacts"
+import Vplayer from "./components/Vplayer.js"
+
+function Body () {
+  return (
+    <>
+        {/*<Music />
+        <Videos />
+        <Shows />
+        <Contacts />*/}
+        <Vplayer />
+    </>
+  )
+}
+
+export default Body
---
+++ src/components/Contacts.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Contacts () {
+  return (
+    <p>contacts</p>
+  )
+}
+
+export default Contacts

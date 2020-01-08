ac5918ac3e31f31a7accccb2fc90571029a2c625
Xavier <xavier.touikan@gmail.com>
2020-01-08 12:56:26 -0500

    Updates

---
+++ precache-manifest.464a9a3bd495236627c3e6349ad1e406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e118d804e4b6107f807cffe5875a0b1",
+    "url": "/react_zenb/index.html"
+  },
+  {
+    "revision": "947532ea97e06dbbdf54",
+    "url": "/react_zenb/static/css/main.f1527b62.chunk.css"
+  },
+  {
+    "revision": "91586d35cac16177e852",
+    "url": "/react_zenb/static/js/2.d180cf32.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react_zenb/static/js/2.d180cf32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "947532ea97e06dbbdf54",
+    "url": "/react_zenb/static/js/main.9f217bd7.chunk.js"
+  },
+  {
+    "revision": "0a46672f3a7ad32290c7",
+    "url": "/react_zenb/static/js/runtime-main.6e75b211.js"
+  }
+]);
